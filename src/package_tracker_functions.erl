7ac373acac4f9b21b0aa9d4663c731ddc5e000a2
denislazo1610 <denislazo1610@gmail.com>
2023-06-14 15:38:50 -0600

    first functions

--- src/package_tracker_functions.erl
+++ src/package_tracker_functions.erl
@@ -1,7 +1,25 @@
 -module(package_tracker_functions).
--export([add/2]).
+-export([add/2, request_location/0, register_package/0, vehicle_location_update/0, mark_deliver/0, put_on_vehicle/0, enter_center/0]).
 % -export([tests/0]).
 
 add(Number1, Number2) ->
     Result = Number1 + Number2,
     Result.
+
+request_location() ->
+    ok.
+
+register_package() ->
+    ok.
+
+vehicle_location_update() ->
+    ok.
+
+mark_deliver() ->
+    ok.
+
+put_on_vehicle() ->
+    ok.
+
+enter_center() ->
+    ok.
